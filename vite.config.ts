19006fe86a23d416f43d0a2ad3f3c8e2dcfcae15
Konerd <adr10won@gmail.com>
2024-10-08 16:30:56 +0900

    chore: Change vite proxy config to only use local web server in local dev environment

--- vite.config.ts
+++ vite.config.ts
@@ -20,35 +20,6 @@ export default ({ mode }) => {
       react(),
       {
         name: 'http-proxy-middleware',
-        config() {
-          return {
-            server: {
-              proxy:
-                mode === 'production' &&
-                process.env.VITE_IS_USE_PROXY === 'true'
-                  ? {
-                      '/api': {
-                        target: process.env.VITE_URL_API_SERVER,
-                        changeOrigin: true,
-                        // rewrite: (path) => path.replace(/^\/api/, ''),
-                      },
-                    }
-                  : {},
-            },
-            preview: {
-              proxy:
-                process.env.VITE_IS_USE_PROXY === 'true'
-                  ? {
-                      '/api': {
-                        target: process.env.VITE_URL_API_SERVER,
-                        changeOrigin: true,
-                        // rewrite: (path) => path.replace(/^\/api/, ''),
-                      },
-                    }
-                  : {},
-            },
-          };
-        },
         configureServer(serverDev: ViteDevServer) {
           if (mode === 'development' && !!app) {
             serverDev.middlewares.use(app);
