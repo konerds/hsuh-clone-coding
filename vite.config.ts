196d8bf9eb9672d8b0343be61df73678c0ab1873
Konerd <adr10won@gmail.com>
2024-10-08 17:29:55 +0900

    chore
    - Change axios base url
    - Update local web server configuration in vite config fiel

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,6 @@ import apiMocker from 'connect-api-mocker';
 import { defineConfig, loadEnv, ViteDevServer } from 'vite';
 import react from '@vitejs/plugin-react';
 
-let app: any;
-
-if (process.env.NODE_ENV === 'development') {
-  import('express').then((express) => {
-    app = express.default();
-    app.use('/api', apiMocker('mocks/api'));
-  });
-}
-
 // https://vitejs.dev/config/
 export default ({ mode }) => {
   Object.assign(process.env, loadEnv(mode, process.cwd()));
@@ -20,8 +11,10 @@ export default ({ mode }) => {
       react(),
       {
         name: 'http-proxy-middleware',
-        configureServer(serverDev: ViteDevServer) {
-          if (mode === 'development' && !!app) {
+        async configureServer(serverDev: ViteDevServer) {
+          if (mode === 'development') {
+            const app = (await import('express')).default();
+            app.use('/api', apiMocker('mocks/api'));
             serverDev.middlewares.use(app);
           }
         },
